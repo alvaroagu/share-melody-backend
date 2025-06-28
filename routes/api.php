1e6fcf7872a36d72bf6125380418106515787f37
alvaroagu <alvaro.aguinagalde@gmail.com>
2025-06-27 20:03:35 -0400

    feat:  project ApiRest

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
+use App\Http\Controllers\portfolio\ProjectController;
 use App\Models\portfolio\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api'])->group(function () {
-    Route::get('/projects', function (Request $request) {
-        // Logic to handle fetching users
-        return Project::orderBy('created_at', 'desc')->get();
-    });
+    Route::apiResource('/projects', ProjectController::class);
+    //Route::apiResource('/danger-identifications', DangerIdentificationController::class);
+    //Route::post('/update/follow-up-controls/{id}', [FollowUpControllController::class, 'updateFollowUpControl']);
 
 });
