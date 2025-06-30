3284096ac0ba10c438c564cd099fe7b144778951
alvaroagu <alvaro.aguinagalde@gmail.com>
2025-06-30 18:25:09 -0400

    fix: missing function in the get method

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api'])->group(function () {
     Route::get('/projects', [ProjectController::class, 'index']);
-    Route::get('/sheet-music', SheetMusicController::class);
+    Route::get('/sheet-music', [SheetMusicController::class, 'index']);
 
 
     //Route::apiResource('/danger-identifications', DangerIdentificationController::class);
